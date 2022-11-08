33e9f0ef2f5d60133a9b63b3e49bc614184350a9
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2022-11-08 09:57:57 +0700

    Add files via upload

---
+++ laba.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	char* buffer = (char*)malloc(1024 * sizeof(char));
+	bool sub = true;
+	int counter = 0;
+	while (sub) {
+		scanf("%c", buffer[counter]);
+		counter++;
+		if (counter % 1024 == 0)
+			buffer = (char*)realloc(buffer, 1024 * (counter / 1024 + 1)   * sizeof(char));
+	}
+	int i = 0;
+	for (; buffer[i] != '\0'; i++);
+	//std::cin >> buffer;
+	cout << buffer[16];
+	return 0;
+}
